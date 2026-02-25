7530ac23e88b4e09e7b2d4a05c49075f63ffea49
James Trivett <James.Trivett@os.uk>
2026-02-25 12:18:03 +0000

    feat: initial sidebar setup

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,46 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <Tabs defaultValue="env" className="w-100">
+        <TabsList>
+          <TabsTrigger value="env">Env</TabsTrigger>
+          <TabsTrigger value="settings">Settings</TabsTrigger>
+        </TabsList>
+        <TabsContent value="env">
+          <form action="">
+            <fieldset>
+              <legend>Dev</legend>
+              <label htmlFor="dev-url">URL</label>
+              <input type="text" id="dev-url" name="dev-url" />
+              <label htmlFor="dev-key">Key</label>
+              <input type="text" id="dev-key" name="dev-key" />
+            </fieldset>
+
+            <fieldset>
+              <legend>Staging</legend>
+              <label htmlFor="staging-url">URL</label>
+              <input type="text" id="staging-url" name="staging-url" />
+              <label htmlFor="staging-key">Key</label>
+              <input type="text" id="staging-key" name="staging-key" />
+            </fieldset>
+
+            <fieldset>
+              <legend>Production</legend>
+              <label htmlFor="prod-url">URL</label>
+              <input type="text" id="prod-url" name="prod-url" />
+              <label htmlFor="prod-key">Key</label>
+              <input type="text" id="prod-key" name="prod-key" />
+            </fieldset>
+          </form>
+        </TabsContent>
+        <TabsContent value="settings">
+          <h2>Lots of dropdowns for changing settings for the map</h2>
+        </TabsContent>
+      </Tabs>
+    </aside>
+  );
+};
+
+export { Sidebar };
