43b358733517d35f672a05cff25e25c2c7ee445b
Mickey Serrapica <mickey.serrapica@os.uk>
2026-02-25 16:01:22 +0000

    feat: added vanilla store

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,56 +1,64 @@
-import { useStore, type TabOptions } from "../stores/global-state";
+import { useGlobalStore, type TabOptions } from "../stores/global-state";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
 import { Dropdown } from "./dropdown";
 
 const Sidebar = () => {
-  const tabs = useStore((state) => state.tab);
-  const setTab = useStore((state) => state.setTab);
+    const tabs = useGlobalStore.use.tab();
+    const setTab = useGlobalStore.use.setTab();
 
-  return (
-    <aside className="max-w-72">
-      <Tabs
-        value={tabs}
-        defaultValue="env"
-        className="w-100"
-        onValueChange={(value) => setTab(value as TabOptions)}
-      >
-        <TabsList>
-          <TabsTrigger value={"env"}>Env</TabsTrigger>
-          <TabsTrigger value={"settings"}>Settings</TabsTrigger>
-        </TabsList>
-        <TabsContent value={"env"}>
-          <form className="prose" action="">
-            <fieldset>
-              <legend>Dev</legend>
-              <label htmlFor="dev-url">URL</label>
-              <input type="text" id="dev-url" name="dev-url" />
-              <label htmlFor="dev-key">Key</label>
-              <input type="text" id="dev-key" name="dev-key" />
-            </fieldset>
+    return (
+        <aside className="max-w-72">
+            <Tabs
+                value={tabs}
+                defaultValue="env"
+                className="w-100"
+                onValueChange={(value) => setTab(value as TabOptions)}
+            >
+                <TabsList>
+                    <TabsTrigger value={"env"}>Env</TabsTrigger>
+                    <TabsTrigger value={"settings"}>Settings</TabsTrigger>
+                </TabsList>
+                <TabsContent value={"env"}>
+                    <form className="prose" action="">
+                        <fieldset>
+                            <legend>Dev</legend>
+                            <label htmlFor="dev-url">URL</label>
+                            <input type="text" id="dev-url" name="dev-url" />
+                            <label htmlFor="dev-key">Key</label>
+                            <input type="text" id="dev-key" name="dev-key" />
+                        </fieldset>
 
-            <fieldset>
-              <legend>Staging</legend>
-              <label htmlFor="staging-url">URL</label>
-              <input type="text" id="staging-url" name="staging-url" />
-              <label htmlFor="staging-key">Key</label>
-              <input type="text" id="staging-key" name="staging-key" />
-            </fieldset>
+                        <fieldset>
+                            <legend>Staging</legend>
+                            <label htmlFor="staging-url">URL</label>
+                            <input
+                                type="text"
+                                id="staging-url"
+                                name="staging-url"
+                            />
+                            <label htmlFor="staging-key">Key</label>
+                            <input
+                                type="text"
+                                id="staging-key"
+                                name="staging-key"
+                            />
+                        </fieldset>
 
-            <fieldset>
-              <legend>Production</legend>
-              <label htmlFor="prod-url">URL</label>
-              <input type="text" id="prod-url" name="prod-url" />
-              <label htmlFor="prod-key">Key</label>
-              <input type="text" id="prod-key" name="prod-key" />
-            </fieldset>
-          </form>
-        </TabsContent>
-        <TabsContent value={"settings"}>
-          <Dropdown />
-        </TabsContent>
-      </Tabs>
-    </aside>
-  );
+                        <fieldset>
+                            <legend>Production</legend>
+                            <label htmlFor="prod-url">URL</label>
+                            <input type="text" id="prod-url" name="prod-url" />
+                            <label htmlFor="prod-key">Key</label>
+                            <input type="text" id="prod-key" name="prod-key" />
+                        </fieldset>
+                    </form>
+                </TabsContent>
+                <TabsContent value={"settings"}>
+                    <Dropdown />
+                </TabsContent>
+            </Tabs>
+        </aside>
+    );
 };
 
 export { Sidebar };
--- src/stores/global-state.ts
+++ src/stores/global-state.ts
@@ -1,51 +1,72 @@
-import { create } from "zustand";
-import { persist, type StateStorage, createJSONStorage } from 'zustand/middleware'
+import { createStore, type StoreApi } from "zustand/vanilla";
+import {
+    persist,
+    type StateStorage,
+    createJSONStorage,
+} from "zustand/middleware";
+import { useStore } from "zustand";
 
 const getUrlSearch = () => {
-  return window.location.search.slice(1)
-}
+    return window.location.search.slice(1);
+};
 
 export type TabOptions = "env" | "settings";
 
 interface GlobalState {
-  tab: TabOptions;
-  setTab: (newTab: TabOptions) => void;
+    tab: TabOptions;
+    setTab: (newTab: TabOptions) => void;
 }
 
+type WithSelectors<S> = S extends { getState: () => infer T }
+    ? S & { use: { [K in keyof T]: () => T[K] } }
+    : never;
+
+const createSelectors = <S extends StoreApi<object>>(_store: S) => {
+    const store = _store as WithSelectors<typeof _store>;
+    store.use = {};
+    for (const k of Object.keys(store.getState())) {
+        (store.use as any)[k] = () =>
+            useStore(_store, (s) => s[k as keyof typeof s]);
+    }
+
+    return store;
+};
 
 const persistentStorage: StateStorage = {
-  getItem: (key): string => {
-    // Check URL first
-      const searchParams = new URLSearchParams(getUrlSearch())
-      const storedValue = searchParams.get(key)
-      return JSON.parse(storedValue as string)
+    getItem: (key): string => {
+        // Check URL first
+        const searchParams = new URLSearchParams(getUrlSearch());
+        const storedValue = searchParams.get(key);
+        return JSON.parse(storedValue as string);
     },
-  setItem: (key, newValue): void => {
-    // Check if query params exist at all, can remove check if always want to set URL
-      const searchParams = new URLSearchParams(getUrlSearch())
-      searchParams.set(key, JSON.stringify(newValue))
-      window.history.replaceState(null, '', `?${searchParams.toString()}`)
-  },
-  removeItem: (key): void => {
-    const searchParams = new URLSearchParams(getUrlSearch())
-    searchParams.delete(key)
-    window.location.search = searchParams.toString()
-  },
-}
+    setItem: (key, newValue): void => {
+        // Check if query params exist at all, can remove check if always want to set URL
+        const searchParams = new URLSearchParams(getUrlSearch());
+        searchParams.set(key, JSON.stringify(newValue));
+        window.history.replaceState(null, "", `?${searchParams.toString()}`);
+    },
+    removeItem: (key): void => {
+        const searchParams = new URLSearchParams(getUrlSearch());
+        searchParams.delete(key);
+        window.location.search = searchParams.toString();
+    },
+};
 
 const storageOptions = {
-  name: 'global-state',
-  storage: createJSONStorage<GlobalState>(() => persistentStorage),
-}
+    name: "global-state",
+    storage: createJSONStorage<GlobalState>(() => persistentStorage),
+};
 
-const useStore = create<GlobalState>()(
-  persist<GlobalState>(
-    (set) => ({
-      tab: "env",
-      setTab: (newTab: "env" | "settings") => set({ tab: newTab }),
-    }),
-    storageOptions,
-  ),
-)
+const globalStore = createStore<GlobalState>()(
+    persist<GlobalState>(
+        (set) => ({
+            tab: "env",
+            setTab: (newTab: "env" | "settings") => set({ tab: newTab }),
+        }),
+        storageOptions,
+    ),
+);
 
-export { useStore };
+const useGlobalStore = createSelectors(globalStore);
+
+export { useGlobalStore, globalStore };
